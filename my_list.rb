53958e5911719a45c4658e5c286a701e329270bb
koomekevo <koome.kevo@gurucrafts.net>
2022-02-25 17:01:50 +0300

    Implement reviewers changes

--- my_list.rb
+++ my_list.rb
@@ -15,12 +15,12 @@ end
 list = MyList.new(1, 2, 3, 4)
 
 # Tests add puts / p infront to view results
-list.all? { |e| e < 5 }
+puts(list.all? { |e| e < 5 })
 
-list.all? { |e| e > 5 }
+puts(list.all? { |e| e > 5 })
 
-list.any? { |e| e == 2 }
+puts(list.any? { |e| e == 2 })
 
-list.any? { |e| e == 5 }
+puts(list.any? { |e| e == 5 })
 
-list.filter(&:even?)
+puts list.filter(&:even?)
